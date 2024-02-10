534f1ea3b01688797f82b8140a3d594339923fe6
yas3jb <yasmu213@gmail.com>
2024-02-10 16:19:55 -0500

    a footer which sticks to the bottom of the page, and has links to all of your <Routes>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <p>React Router</p>
+      <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+        Home
+      </Link>
+      <Link to="/blue" style={{ textDecoration: "none", color: "black" }}>
+        Blue
+      </Link>
+      <Link to="/red" style={{ textDecoration: "none", color: "black" }}>
+        Red
+      </Link>
     </div>
   );
 }

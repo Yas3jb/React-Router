7a68150384fa64b82db335f604ab0fd215bc8e0b
yas3jb <yasmu213@gmail.com>
2024-02-10 15:58:07 -0500

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
-import Blue from "./components/Blue";
-import Red from "./components/Red";
-import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <div id="container">
-        <div id="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-        </div>
-        <div id="main-section">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-          </Routes>
-        </div>
-      </div>
-    </>
+    <div id="container">
+      <Navbar />
+      <Container />
+      <Footer />
+    </div>
   );
 }
 

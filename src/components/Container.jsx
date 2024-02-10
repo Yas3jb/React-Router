e2dcd6451d161dcb9b0a84adbd234d80dd79dc38
yas3jb <yasmu213@gmail.com>
2024-02-10 15:55:53 -0500

    create container component and add routes

---
+++ src/components/Container.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Blue from "./Blue";
+import Red from "./Red";
+
+export default function Container() {
+  return (
+    <div id="main-section">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blue" element={<Blue />} />
+        <Route path="/red" element={<Red />} />
+      </Routes>
+    </div>
+  );
+}
